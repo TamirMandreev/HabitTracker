87a019f15122f1b7c256d73cb4fd359c08be8756
Tamir Mandreev <tamirmandreev@mail.ru>
2025-04-23 02:20:31 +0800

    Реализовал CRUD для модели Habit

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
+from tracker import views
 from tracker.apps import TrackerConfig
 
 app_name = TrackerConfig.name
 
 urlpatterns = [
-
+    path('habits/create/', views.HabitCreateAPIView.as_view(), name='habit-create'),
+    path('habits/', views.HabitListAPIView.as_view(), name='habit-list'),
+    path('habits/detail/<int:pk>/', views.HabitDetailAPIView.as_view(), name='habit-detail'),
+    path('habits/update/<int:pk>/', views.HabitUpdateAPIView.as_view(), name='habit-update'),
+    path('habits/delete/<int:pk>/', views.HabitDeleteAPIView.as_view(), name='habit-delete'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+
+from tracker.models import Habit
+from tracker.serializers import HabitSerializer
+
 
 # Create your views here.
+
+class HabitCreateAPIView(CreateAPIView):
+    serializer_class = HabitSerializer
+
+
+class HabitListAPIView(ListAPIView):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+
+
+class HabitDetailAPIView(RetrieveAPIView):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+
+
+class HabitUpdateAPIView(UpdateAPIView):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+
+
+class HabitDeleteAPIView(DestroyAPIView):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
